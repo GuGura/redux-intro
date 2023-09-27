2ead0349f3f86e19d9335d87db20b4eb78b6050e
King <wodus331@gmail.com>
2023-09-27 16:59:31 +0900

    change redux-tools use middleware

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,5 +1,6 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+const host = 'api.frankfurter.app';
 const initialState = {
     balance: 0,
     loan: 0,
@@ -12,6 +13,7 @@ const accountSlice = createSlice({
     reducers: {
         deposit(state, action) {
             state.balance += action.payload;
+            state.isLoading = false;
         },
         withdraw(state, action) {
             state.balance -= action.payload;
@@ -32,10 +34,26 @@ const accountSlice = createSlice({
             state.loan = 0;
             state.loanPurpose = '';
             state.balance -= state.loan;
+        },
+        convertingCurrency(state){
+            state.isLoading = true
         }
     }
 })
-export const {deposit, payLoan, requestLoan, withdraw} = accountSlice.actions
+export const {payLoan, requestLoan, withdraw} = accountSlice.actions
+
+export function deposit(amount, currency) {
+    if (currency === 'USD')
+        return {type: 'account/deposit', payload: amount}
+
+    return async (dispatch, getState) => {
+        dispatch({type: 'account/convertingCurrency'})
+        //API CALL
+        const res = await fetch(`https://${host}/latest?amount=${amount}&from=${currency}&to=USD`)
+        const data = await res.json();
+        const converted = data.rates.USD
+        dispatch({type: 'account/deposit', payload: converted})
+    }
+}
+
 export default accountSlice.reducer;
-
-
